c79742494645f8727d671f7d95957bc2c04c109c
Blessed Sibanda <blessedsibanda434@gmail.com>
2018-11-10 12:00:23 +0200

    Initial Commit

---
+++ store.js
@@ -0,0 +1,21 @@
+state = {
+    recipes: [
+        {
+            id: 10,
+            name: 'omelette'
+        }
+    ],
+    ingredients: [
+        {
+            recipe_id: 10,
+            name: 'eggs',
+            quantity: 2
+        },
+        {
+            recipe_id: 10,
+            name: 'milk',
+            quantity: 1
+        }
+    ]
+};
+console.log(state);
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+    context: path.resolve(__dirname, "src"),
+    mode: 'development',
+    entry: {
+        app: './index.js'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, "dist/assets/media"),
+        stats: 'errors-only',
+        open: true,
+        port: 3000,
+        compress: true
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            // babel-loader
+            {
+                test: /\.js$/,
+                include: /src/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            // html-loader
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        })
+    ]
+}
+
+module.exports = config;
